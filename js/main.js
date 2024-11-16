c51f51b641f1e578124224f7e8964cf451a86bdc
Jcambareri6 <joaquin.cambareri@eest3necochea.edu.ar>
2024-11-16 18:29:25 -0300

    parallax seccion 1 funcionando

--- js/main.js
+++ js/main.js
@@ -6,25 +6,53 @@ let anchoInicial = 560;
 let altoInicial = 320;
 let anchoFinal=150;
 let alturaFinal=86;
-let scrollLimite= 300;
+let scrollLimite= 200;
 window.addEventListener("scroll", function() {
-    const scrollTop = Math.min(window.scrollY, scrollLimite);
+    const scrollEvento = window.scrollY;
+    // punto 4 
+    animarHeader(scrollEvento)
+    //parallax seccion 1 
+    animarParallaxSeccion1(scrollEvento)
 
-    // Calcular el tamaño actual del logo en función del scroll
-    const anchoActual = anchoInicial - (anchoInicial - anchoFinal) * (scrollTop / scrollLimite);
-    
-    // Calcular el desplazamiento hacia arriba del logo
-    const topActual = Math.min(scrollTop, 140); // El logo no sube más de 50px, ajusta este valor como desees
+});
+function animarHeader(scrollY){
+    let scrollTop = Math.min(scrollY, scrollLimite);
+    let anchoActual = anchoInicial - (anchoInicial - anchoFinal) * (scrollTop / scrollLimite); // calcula porc para achicar img
+    let topActual = Math.min(scrollTop, 150); 
 
-    // Aplicar los nuevos valores al logo
     logoHeader.style.width = `${anchoActual}px`;
     logoHeader.style.transform = `translateY(-${topActual}px)`;
-   
-
     if(window.scrollY>=scrollLimite){
-       
-
-        // Asegurarte de que tenga 'headerScroll'
         header.classList.add('headerScroll');
     }
-});
+}
+ function animarParallaxSeccion1(scrollY){
+    let elementosPrimerPlano = document.querySelectorAll("[data-primerPlano]");
+    let elementosFondoIntermedio = document.querySelectorAll("[data-fondoIntermedio]");
+    let elementosFondo = document.querySelectorAll("[data-planoLejano]");
+    let initScroll =170
+    let limitScroll= 270
+    let rangoScroll = limitScroll-initScroll;
+    let maxMovimiento= 0
+
+    elementosPrimerPlano.forEach(elementoPrimerPlano =>{
+        mover(scrollY,initScroll,limitScroll,elementoPrimerPlano,"data-primerPlano");
+    })
+    elementosFondoIntermedio.forEach(elemento =>{
+        console.log(elemento)
+        mover(scrollY,initScroll,limitScroll,elemento, "data-fondoIntermedio");
+    })
+    elementosFondo.forEach(elemento =>{
+        console.log(elemento)
+        mover(scrollY,initScroll,limitScroll,elemento, "data-planoLejano");
+    })
+}
+ function mover(scrollY,initScroll,limitScroll,elemento, data){
+   
+    if (scrollY >= initScroll && scrollY <= limitScroll) {
+        let velocidad = parseFloat(elemento.getAttribute(data));
+        let movimiento =  (scrollY-initScroll)*velocidad
+        elemento.style.transform = `translateY(${movimiento}px)`;
+ 
+     }
+}
