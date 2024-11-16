db0dd6ba0f465d7f9bf1e2dfe10cea02bb87c9b2
Jcambareri6 <joaquin.cambareri@eest3necochea.edu.ar>
2024-11-16 12:47:08 -0300

    realizada la primer animacion

--- js/main.js
+++ js/main.js
@@ -6,14 +6,25 @@ let anchoInicial = 560;
 let altoInicial = 320;
 let anchoFinal=150;
 let alturaFinal=86;
-let scrollLimite= 600;
+let scrollLimite= 300;
 window.addEventListener("scroll", function() {
     const scrollTop = Math.min(window.scrollY, scrollLimite);
 
-    
+    // Calcular el tamaño actual del logo en función del scroll
     const anchoActual = anchoInicial - (anchoInicial - anchoFinal) * (scrollTop / scrollLimite);
-
-    logoHeader.style.width = `${anchoActual}px`; 
     
+    // Calcular el desplazamiento hacia arriba del logo
+    const topActual = Math.min(scrollTop, 140); // El logo no sube más de 50px, ajusta este valor como desees
 
+    // Aplicar los nuevos valores al logo
+    logoHeader.style.width = `${anchoActual}px`;
+    logoHeader.style.transform = `translateY(-${topActual}px)`;
+   
+
+    if(window.scrollY>=scrollLimite){
+       
+
+        // Asegurarte de que tenga 'headerScroll'
+        header.classList.add('headerScroll');
+    }
 });
