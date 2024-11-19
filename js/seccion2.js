37d4a3523ebcc0a7cf87072dbe8e5eff8220d0be
Valentino <malassisivalen@gmail.com>
2024-11-19 18:38:36 -0300

    a

--- js/seccion2.js
+++ js/seccion2.js
@@ -5,16 +5,29 @@ const partes = document.querySelector('.partesJuego');
 const figura4 = document.querySelector('.figura4');
 const figura5 = document.querySelector('.figura5');
 
+
+
+const iframe = document.getElementById('videosec6');
+const figura = document.getElementById('figurasec6');
+const seccion6 = document.querySelector(".seccion6");
+
 // Función que mueve los elementos al hacer scroll
 window.addEventListener('scroll', () => {
     let scrollPosition = window.scrollY; // Obtenemos la posición del scroll
  
-    titulo.style.transform = `translateY(${scrollPosition * 0.13}px)`; 
-    parrafo.style.transform = `translateY(${scrollPosition * 0.13}px)`; 
-    partes.style.transform = `translateY(${scrollPosition * 0.12}px)`; 
+    titulo.style.transform = `translateY(${scrollPosition * 0.1}px)`; 
+    parrafo.style.transform = `translateY(${scrollPosition * 0.1}px)`; 
+    partes.style.transform = `translateY(${scrollPosition * 0.1}px)translateX(${scrollPosition * -0.1}px)`; 
 
     figura4.style.transform = `translateY(${scrollPosition * 0.06}px)translateX(${scrollPosition * -0.03}px)`; 
     figura5.style.transform = `translateY(${scrollPosition * 0.14}px)`; 
+
+    const seccionheight = seccion6.getBoundingClientRect().height;
+    const seccionwidth = seccion6.getBoundingClientRect().width;
+
+
+    iframe.style.transform = `translateY(${scrollPosition * 0.007}px) translateX(${scrollPosition * -0.008}px)`; 
+    figura.style.transform = `translateY(${scrollPosition * -0.002}px) translateX(${scrollPosition * 0.004}px)`; 
 })
 
 
